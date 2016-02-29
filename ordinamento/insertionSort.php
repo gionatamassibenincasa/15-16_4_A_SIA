ef1b1c5423d9ba95bc73942b3db6761a483eca2e
gm_at_benincasa <gionata.massi@savoiabenincasa.it>
2016-02-29 08:43:47 +0000

    insertionSort

---
+++ ordinamento/insertionSort.php
@@ -0,0 +1,28 @@
+<?php
+
+function insertionSort(&$a) {
+    for($posElementoDaInserire = 1;
+        $posElementoDaInserire < count($a);
+        $posElementoDaInserire++)
+    {
+        echo "Inizio iterazione n. $posElementoDaInserire<br>\n";
+        print_r($a);
+
+        if ($a[$posElementoDaInserire] >=
+            $a[$posElementoDaInserire - 1])
+                continue;
+        $elementoDaInserire = $a[$posElementoDaInserire];
+        $i = $posElementoDaInserire - 1;
+        while ($i >= 0 && $a[$i] > $elementoDaInserire) {
+            $a[$i+1] = $a[$i];
+            $i--;
+        }
+        $a[$i+1] = $elementoDaInserire;
+    }
+}
+
+$vettore = array(3,7,5,1,4);
+echo "Vettore non ordinato<br>\n";
+print_r($vettore);
+insertionSort($vettore);
+?>
